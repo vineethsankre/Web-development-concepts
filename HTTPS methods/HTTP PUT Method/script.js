adec0ab8f5baa0b150f13550c1bf6d3fe12073c2
vineeth2207 <lsvineethsankre@gmail.com>
2024-01-21 20:10:23 +0530

    HTTP PUT code posted

---
+++ HTTPS methods/HTTP PUT Method/script.js
@@ -0,0 +1,41 @@
+let userInputEl = document.getElementById("userInput");
+let requestBodyEl = document.getElementById("requestBody");
+let sendPutRequestBtnEl = document.getElementById("sendPutRequestBtn");
+let requestStatusEl = document.getElementById("requestStatus");
+let httpResponseEl = document.getElementById("httpResponse");
+let loadingEl = document.getElementById("loading");
+
+function sendPutRequest() {
+    let requestBody = requestBodyEl.value;
+    let userId = userInputEl.value;
+    let url = "https://gorest.co.in/public-api/users";
+    loadingEl.classList.remove("d-none");
+    let options = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: "Bearer b10dc007b1abd36777f363fa7ac1f6e856e106003dd79ce24e3d766247e5b227"
+        },
+        body: requestBody
+    };
+
+    loadingEl.classList.remove("d-none");
+    requestStatusEl.classList.add("d-none");
+
+    fetch(url, options)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(jsonData) {
+            loadingEl.classList.add("d-none");
+            requestStatusEl.classList.remove("d-none");
+
+            let requestStatus = jsonData.code;
+            let responseBody = JSON.stringify(jsonData);
+            requestStatusEl.textContent = requestStatus;
+            httpResponseEl.textContent = responseBody;
+        });
+}
+
+sendPutRequestBtnEl.addEventListener("click", sendPutRequest);
